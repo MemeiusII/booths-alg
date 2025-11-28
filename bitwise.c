affad6bf27824d44d3b0b387463fb85e725cf7d1
MemeiusII <ewbilbrey@gmail.com>
2025-11-27 21:51:53 -0600

    Working alg

--- bitwise.c
+++ bitwise.c
@@ -13,44 +13,50 @@ print_bits (size_t size, uint16_t byte)
     for (int i = size - 1; i >= 0; i--)
     {
         printf ("%d", (byte >> i) & 1);
+        if (i % 8 == 0)
+            printf(" ");
     }
 }
 
 void
 next_iter(uint16_t *product, 
-          int8_t   multiplicand,
+          uint8_t  multiplicand,
           bool     *flag)
 {
     bool product_bit = (*product & FLAG_MASK);
 
     if (!product_bit && *flag) {
-        int8_t top = (int8_t)(*product >> 8);
-        int8_t res = top + multiplicand;
-        *product = (*product & 0x00FF) | (((uint16_t)(uint8_t)res) << 8);
+        printf ("Add\n");
+        uint8_t top = *product >> 8;
+        uint8_t res = top + multiplicand;
+        *product = (*product & ~PRODUCT_MASK) | ((uint16_t) res) << 8;
     }
     else if (product_bit && !*flag) {
-        int8_t top = (int8_t)(*product >> 8);
-        int8_t res = top - multiplicand;
-        *product = (*product & 0x00FF) | (((uint16_t)(uint8_t)res) << 8);
+        printf ("Sub\n");
+        uint8_t top = *product >> 8;
+        uint8_t res = top - multiplicand;
+        *product = (*product & ~PRODUCT_MASK) | ((uint16_t) res) << 8;
     }
+    else
+        printf ("Nop\n");
 
     *flag = product_bit;
-
     // Arithmetic right shift
+    printf ("Shift Right\n");
     *product = (uint16_t)( (int16_t)(*product) >> 1 );
 }
 
 int8_t
-booths_alg (uint16_t x, int8_t y)
+booths_alg (int8_t x, int8_t y)
 {
-    print_bits (16, x);
+    print_bits (8, x);
     printf ("\n%d\n", x);
     print_bits (8, y);
     printf ("\n%d\n", y);
 
-    uint16_t product    = x;
-    int8_t multiplicand = y;
-    bool   flag         = 0;
+    uint16_t product      = (uint16_t) x & ~PRODUCT_MASK; // Set leading zeros
+    int8_t   multiplicand = y;
+    bool     flag         = 0;
 
     printf ("Initialize\n");
     printf ("Product: ");
@@ -79,8 +85,7 @@ booths_alg (uint16_t x, int8_t y)
 
     }
 
-    /* Return 8 least significant bits */
-    return (int8_t) (product >> 8);
+    return (int8_t) (product);
 }
 
 int
@@ -94,8 +99,9 @@ main ()
     printf ("Enter another integer: ");
     scanf ("%d", &y);
 
-    int8_t result = booths_alg ((uint16_t) y, (int8_t) x);
-    printf ("Result: %d", result);
+    int8_t result = booths_alg ((uint8_t) y, (uint8_t) x);
+    printf ("Result: %d\n", (int)result);
+    print_bits (8, result);
 
     return 0;
 }
